f56e8fab3ce95fe081dd3c47256ab0201eae9494
TUNG N KIM <Tungkims@gmail.com>
2023-02-08 21:24:20 -0600

    Added test code for insert and select

--- EntityFrameworkCoreExample/Program.cs
+++ EntityFrameworkCoreExample/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkCoreExample;
+
+using StudentContext dbContext = new ();
+
+// Add with EF Core
+Student s = new()
+{
+    FullName = "Tung Kim",
+    EmailAddress = "tung.kim@gmail.com",
+    DateOfBirth = new DateTime(2000, 1, 1)
+};
+
+dbContext.Students.Add (s); // prepares the Student insert statement
+dbContext.SaveChanges(); // executes pending insert. (In actuality: Execute any pending insert/update/delete query)
+
+// Retrieve Students from DB
+List<Student> allStudents = dbContext.Students.ToList(); // Method syntax
+/*
+allStudents = (from stu in dbContext.Students
+               select stu).ToList(); // Query syntax
+*/
+
+foreach (Student stu in allStudents)
+{
+    Console.WriteLine($"{stu.FullName} has an email of {stu.EmailAddress}");
+    Console.WriteLine();
+}
